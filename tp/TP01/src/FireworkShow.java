574ad6f827d09931f740718626404e99c53c0690
Nassim <101125927+Nassim-H@users.noreply.github.com>
2023-09-04 17:13:33 +0200

    Ajout Singleton BeesFirework

--- tp/TP01/src/FireworkShow.java
+++ tp/TP01/src/FireworkShow.java
@@ -34,7 +34,7 @@ class Firework {
     /**
      * La couleur du feu d'artifice.
      */
-    String color;
+    Color color;
 
     /**
      * Si le feu d'artifice explose deux fois.
@@ -48,7 +48,7 @@ class Firework {
      * @param color La couleur du feu d'artifice.
      * @param isDouble Si le feu d'artifice explose deux fois.
      */
-    public Firework(int size, String color, boolean isDouble) {
+    public Firework(int size, Color color, boolean isDouble) {
         this.size = size;
         this.color = color;
         this.isDouble = isDouble;
@@ -56,27 +56,7 @@ class Firework {
 
 }
 
-/**
- * La classe BeesFirework représente un type particulier de feux d'artifice,
- * qui se comportent comme un groupe d'abeilles.
- *
- * @author Romain Wallon
- *
- * @version 0.1.0
- */
-class BeesFirework {
 
-    /**
-     * Donne la chaîne de caractères à afficher pour représenter l'explosion
-     * de ce feu d'artifice.
-     *
-     * @return La chaîne de caractères représentant l'explosion.
-     */
-    public String getBeesExplosionString() {
-        return "Bzzzzzz!";
-    }
-
-}
 
 /**
  * La classe FireworkShow permet de simuler un spectacle de feux d'artifice.
@@ -109,138 +89,140 @@ public class FireworkShow {
 
     /**
      * Lance un feu d'artifice.
-     * 
+     *
      * @param firework Le feu d'artifice à lancer.
      */
     static void launchFirework(Firework firework) {
         // On change l'affichage dans la console pour avoir la couleur appropriée.
-        if ("RED".equals(firework.color)) {
-            System.out.print("\033[0;31m");
-            
-        } else if ("GREEN".equals(firework.color)) {
-            System.out.print("\033[0;32m");
-            
-        } else if ("BLUE".equals(firework.color)) {
-            System.out.print("\033[0;34m");
-            
-        } else if ("YELLOW".equals(firework.color)) {
-            System.out.print("\033[0;33m");
-            
-        } else {
-            System.out.print("\033[0;37m");
+        switch (firework.color) {
+            case RED:
+                System.out.print("\033[0;31m");
+                break;
+            case GREEN:
+                System.out.print("\033[0;32m");
+                break;
+            case BLUE:
+                System.out.print("\033[0;34m");
+                break;
+            case YELLOW:
+                System.out.print("\033[0;33m");
+                break;
+            default:
+                System.out.print("\033[0;37m");
+                break;
         }
-        
-        // On affiche le feu d'artifice.
-        try {
-            switch (firework.size) {
-                case FAILED:
-                    // Ce feu d'artifice est hors service.
-                    System.out.print("pschit");
-                    break;
+            // On affiche le feu d'artifice.
+            try {
+                switch (firework.size) {
+                    case FAILED:
+                        // Ce feu d'artifice est hors service.
+                        System.out.print("pschit");
+                        break;
 
-                case SMALL:
-                    // C'est un petit feu d'artifice.
-                    System.out.print("boum !");
-                    if (firework.isDouble) System.out.print(" boum !");
-                    Thread.sleep(500);
-                    break;
+                    case SMALL:
+                        // C'est un petit feu d'artifice.
+                        System.out.print("boum !");
+                        if (firework.isDouble) System.out.print(" boum !");
+                        Thread.sleep(500);
+                        break;
 
-                case MEDIUM:
-                    // C'est un feu d'artifice moyen.
-                    System.out.print("Boouum !!");
-                    if (firework.isDouble) System.out.print(" Boouum !!");
-                    Thread.sleep(1000);
-                    break;
+                    case MEDIUM:
+                        // C'est un feu d'artifice moyen.
+                        System.out.print("Boouum !!");
+                        if (firework.isDouble) System.out.print(" Boouum !!");
+                        Thread.sleep(1000);
+                        break;
 
-                default:
-                    // C'est un grand feu d'artifice.
-                    System.out.print("BOOOOUUUUM !!!!");
-                    if (firework.isDouble) System.out.print(" BOOOOUUUUM !!!!");
-                    Thread.sleep(1500);
-                    break;
+                    default:
+                        // C'est un grand feu d'artifice.
+                        System.out.print("BOOOOUUUUM !!!!");
+                        if (firework.isDouble) System.out.print(" BOOOOUUUUM !!!!");
+                        Thread.sleep(1500);
+                        break;
+                }
+
+            } catch (InterruptedException e) {
+                // Ceci ne doit jamais se produire.
+                e.printStackTrace();
             }
-            
-        } catch (InterruptedException e) {
-            // Ceci ne doit jamais se produire.
-            e.printStackTrace();
+
+            // On remet la console dans son état d'origine.
+            System.out.println("\033[0m");
         }
-       
-        // On remet la console dans son état d'origine.
-        System.out.println("\033[0m");
-    }
-    
-    /**
-     * Démarre le programme.
-     *
-     * @param args Les arguments de la ligne de commande.
-     */
-    public static void main(String[] args) {
-        ArrayList<Object> fireworks = new ArrayList<Object>();
 
-        fireworks.add(new Firework(FAILED, "RED", true));
-        fireworks.add(new Firework(FAILED, "GREEN", true));
-        fireworks.add(new Firework(FAILED, "BLUE", true));
-        fireworks.add(new Firework(FAILED, "YELLOW", true));
-        fireworks.add(new Firework(FAILED, "WHITE", true));
-        
-        fireworks.add(new Firework(FAILED, "RED", false));
-        fireworks.add(new Firework(FAILED, "GREEN", false));
-        fireworks.add(new Firework(FAILED, "BLUE", false));
-        fireworks.add(new Firework(FAILED, "YELLOW", false));
-        fireworks.add(new Firework(FAILED, "WHITE", false));
-        
-        fireworks.add(new Firework(SMALL, "RED", true));
-        fireworks.add(new Firework(SMALL, "GREEN", true));
-        fireworks.add(new Firework(SMALL, "BLUE", true));
-        fireworks.add(new Firework(SMALL, "YELLOW", true));
-        fireworks.add(new Firework(SMALL, "WHITE", true));
-        
-        fireworks.add(new Firework(SMALL, "RED", false));
-        fireworks.add(new Firework(SMALL, "GREEN", false));
-        fireworks.add(new Firework(SMALL, "BLUE", false));
-        fireworks.add(new Firework(SMALL, "YELLOW", false));
-        fireworks.add(new Firework(SMALL, "WHITE", false));
-        
-        fireworks.add(new Firework(MEDIUM, "RED", true));
-        fireworks.add(new Firework(MEDIUM, "GREEN", true));
-        fireworks.add(new Firework(MEDIUM, "BLUE", true));
-        fireworks.add(new Firework(MEDIUM, "YELLOW", true));
-        fireworks.add(new Firework(MEDIUM, "WHITE", true));
-        
-        fireworks.add(new Firework(MEDIUM, "RED", false));
-        fireworks.add(new Firework(MEDIUM, "GREEN", false));
-        fireworks.add(new Firework(MEDIUM, "BLUE", false));
-        fireworks.add(new Firework(MEDIUM, "YELLOW", false));
-        fireworks.add(new Firework(MEDIUM, "WHITE", false));
-        
-        fireworks.add(new Firework(BIG, "RED", true));
-        fireworks.add(new Firework(BIG, "GREEN", true));
-        fireworks.add(new Firework(BIG, "BLUE", true));
-        fireworks.add(new Firework(BIG, "YELLOW", true));
-        fireworks.add(new Firework(BIG, "WHITE", true));
-        
-        fireworks.add(new Firework(BIG, "RED", false));
-        fireworks.add(new Firework(BIG, "GREEN", false));
-        fireworks.add(new Firework(BIG, "BLUE", false));
-        fireworks.add(new Firework(BIG, "YELLOW", false));
-        fireworks.add(new Firework(BIG, "WHITE", false));
-        
-        fireworks.add(new BeesFirework());
-        fireworks.add(new BeesFirework());
-        fireworks.add(new BeesFirework());
-        fireworks.add(new BeesFirework());
-        fireworks.add(new BeesFirework());
+        /**
+         * Démarre le programme.
+         *
+         * @param args Les arguments de la ligne de commande.
+         */
+        public static void main (String[]args){
+            ArrayList<Object> fireworks = new ArrayList<Object>();
 
-        Collections.shuffle(fireworks);
-        
-        for (int i = 0; i < fireworks.size(); i++) {
-            Object firework = fireworks.get(i);
-            if (firework instanceof Firework) {
-                launchFirework((Firework) firework);
-            } else {
-                System.out.println(((BeesFirework) firework).getBeesExplosionString());
+            fireworks.add(new Firework(FAILED, Color.RED, true));
+            fireworks.add(new Firework(FAILED, Color.GREEN, true));
+            fireworks.add(new Firework(FAILED, Color.BLUE, true));
+            fireworks.add(new Firework(FAILED, Color.YELLOW, true));
+            fireworks.add(new Firework(FAILED, Color.WHITE, true));
+
+            fireworks.add(new Firework(FAILED, Color.RED, false));
+            fireworks.add(new Firework(FAILED, Color.GREEN, false));
+            fireworks.add(new Firework(FAILED, Color.BLUE, false));
+            fireworks.add(new Firework(FAILED, Color.YELLOW, false));
+            fireworks.add(new Firework(FAILED, Color.WHITE, false));
+
+            fireworks.add(new Firework(SMALL, Color.RED, true));
+            fireworks.add(new Firework(SMALL, Color.GREEN, true));
+            fireworks.add(new Firework(SMALL, Color.BLUE, true));
+            fireworks.add(new Firework(SMALL, Color.YELLOW, true));
+            fireworks.add(new Firework(SMALL, Color.WHITE, true));
+
+            fireworks.add(new Firework(SMALL, Color.RED, false));
+            fireworks.add(new Firework(SMALL, Color.GREEN, false));
+            fireworks.add(new Firework(SMALL, Color.BLUE, false));
+            fireworks.add(new Firework(SMALL, Color.YELLOW, false));
+            fireworks.add(new Firework(SMALL, Color.WHITE, false));
+
+            fireworks.add(new Firework(MEDIUM, Color.RED, true));
+            fireworks.add(new Firework(MEDIUM, Color.GREEN, true));
+            fireworks.add(new Firework(MEDIUM, Color.BLUE, true));
+            fireworks.add(new Firework(MEDIUM, Color.YELLOW, true));
+            fireworks.add(new Firework(MEDIUM, Color.WHITE, true));
+
+            fireworks.add(new Firework(MEDIUM, Color.RED, false));
+            fireworks.add(new Firework(MEDIUM, Color.GREEN, false));
+            fireworks.add(new Firework(MEDIUM, Color.BLUE, false));
+            fireworks.add(new Firework(MEDIUM, Color.YELLOW, false));
+            fireworks.add(new Firework(MEDIUM, Color.WHITE, false));
+
+            fireworks.add(new Firework(BIG, Color.RED, true));
+            fireworks.add(new Firework(BIG, Color.GREEN, true));
+            fireworks.add(new Firework(BIG, Color.BLUE, true));
+            fireworks.add(new Firework(BIG, Color.YELLOW, true));
+            fireworks.add(new Firework(BIG, Color.WHITE, true));
+
+            fireworks.add(new Firework(BIG, Color.RED, false));
+            fireworks.add(new Firework(BIG, Color.GREEN, false));
+            fireworks.add(new Firework(BIG, Color.BLUE, false));
+            fireworks.add(new Firework(BIG, Color.YELLOW, false));
+            fireworks.add(new Firework(BIG, Color.WHITE, false));
+
+            fireworks.add(new BeesFirework());
+            fireworks.add(new BeesFirework());
+            fireworks.add(new BeesFirework());
+            fireworks.add(new BeesFirework());
+            fireworks.add(new BeesFirework());
+
+            Collections.shuffle(fireworks);
+
+            for (int i = 0; i < fireworks.size(); i++) {
+                Object firework = fireworks.get(i);
+                if (firework instanceof Firework) {
+                    launchFirework((Firework) firework);
+                } else {
+                    System.out.println(((BeesFirework) firework).getBeesExplosionString());
+                }
             }
         }
+
     }
 
-}
